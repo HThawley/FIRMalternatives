342f48e86fbb320f821a978b1b6732e065f87dc9
Harry Thawley <Harry.Thawley@anu.edu.au>
2024-10-18 14:41:19 +1100

    minor speed improvement

--- Network.py
+++ Network.py
@@ -9,12 +9,13 @@ from numba import njit
 @njit()
 def Transmission(solution, output=False):
     
-    MPeak = np.atleast_2d(solution.flexible).T * np.atleast_2d(solution.CPeak / solution.CPeak.sum())
-    MDeficit = np.atleast_2d(solution.Deficit).T * np.divide(solution.MLoad, solution.MLoad.sum(axis=1).reshape(-1, 1))
+    solution.MPeak = np.atleast_2d(solution.flexible).T * np.atleast_2d(solution.CPeak / solution.CPeak.sum())
+    solution.MDeficit = np.atleast_2d(solution.Deficit).T * np.divide(solution.MLoad, solution.MLoad.sum(axis=1).reshape(-1, 1))
 
     MPW = solution.GPV + solution.GWind
-    MSpillage = np.atleast_2d(solution.Spillage).T * np.divide(MPW, np.atleast_2d(MPW.sum(axis=1) + 0.00000001).T) 
-
+    solution.MSpillage = np.atleast_2d(solution.Spillage).T * np.divide(MPW, np.atleast_2d(MPW.sum(axis=1) + 0.00000001).T) 
+    solution.MPV, solution.MWind = solution.GPV, solution.GWind
+    
     # dzsm = solution.CPHP != 0 # divide by zero safe mask
     # pcfactor = np.zeros(solution.CPHP.shape)
     # pcfactor[dzsm] =  solution.CPHP[dzsm] / solution.CPHP[dzsm].sum(axis=0)
@@ -22,28 +23,21 @@ def Transmission(solution, output=False):
     # seems to handle divide by zero ok - but leaving above code for later dev
     pcfactor =  solution.CPHP / solution.CPHP.sum(axis=0)
     
-    MDischarge = (np.atleast_2d(solution.Discharge).T * pcfactor)# MDischarge: DPH(j, t)
-    MCharge = (np.atleast_2d(solution.Charge).T * pcfactor) # MCharge: CHPH(j, t)
+    solution.MDischarge = (np.atleast_2d(solution.Discharge).T * pcfactor)# MDischarge: DPH(j, t)
+    solution.MCharge = (np.atleast_2d(solution.Charge).T * pcfactor) # MCharge: CHPH(j, t)
+    solution.MStorage = np.atleast_2d(solution.Storage).T * pcfactor # SPH(t, j), MWh
 
-    MImport = solution.MLoad + MCharge + MSpillage \
-              - MPW - solution.GBaseload - MPeak - MDischarge - MDeficit # EIM(t, j), MW
+    MImport = solution.MLoad + solution.MCharge + solution.MSpillage \
+              - MPW - solution.GBaseload - solution.MPeak - solution.MDischarge - solution.MDeficit # EIM(t, j), MW
 
-    FQ = -1 * MImport[:, np.where(solution.Nodel_int==0)[0][0]] if 0 in solution.Nodel_int else np.zeros(solution.intervals, dtype=np.float64)
-    AS = -1 * MImport[:, np.where(solution.Nodel_int==2)[0][0]] if 2 in solution.Nodel_int else np.zeros(solution.intervals, dtype=np.float64)
-    SW = MImport[:, np.where(solution.Nodel_int==7)[0][0]] if 7 in solution.Nodel_int else np.zeros(solution.intervals, dtype=np.float64)
-    TV = -1 * MImport[:, np.where(solution.Nodel_int==5)[0][0]]
+    solution.FQ = -1 * MImport[:, np.where(solution.Nodel_int==0)[0][0]] if 0 in solution.Nodel_int else np.zeros(solution.intervals, dtype=np.float64)
+    solution.AS = -1 * MImport[:, np.where(solution.Nodel_int==2)[0][0]] if 2 in solution.Nodel_int else np.zeros(solution.intervals, dtype=np.float64)
+    solution.SW = MImport[:, np.where(solution.Nodel_int==7)[0][0]] if 7 in solution.Nodel_int else np.zeros(solution.intervals, dtype=np.float64)
+    solution.TV = -1 * MImport[:, np.where(solution.Nodel_int==5)[0][0]]
 
-    NQ = MImport[:, np.where(solution.Nodel_int==3)[0][0]] - FQ
-    NV = MImport[:, np.where(solution.Nodel_int==6)[0][0]] - TV
+    solution.NQ = MImport[:, np.where(solution.Nodel_int==3)[0][0]] - solution.FQ
+    solution.NV = MImport[:, np.where(solution.Nodel_int==6)[0][0]] - solution.TV
 
-    NS = -1. * MImport[:, np.where(solution.Nodel_int==1)[0][0]] - NQ - NV
-    
-    assert np.abs(NS - MImport[:, np.where(solution.Nodel_int==4)[0][0]] - AS + SW).sum()<=1
+    solution.NS = -1. * MImport[:, np.where(solution.Nodel_int==1)[0][0]] - solution.NQ - solution.NV
 
-    if output is True:
-        MStorage = np.atleast_2d(solution.Storage).T * pcfactor # SPH(t, j), MWh
-        solution.MPV, solution.MWind, solution.MBaseload, solution.MPeak = (solution.GPV, solution.GWind, solution.GBaseload, MPeak)
-        solution.MDischarge, solution.MCharge, solution.MStorage = (MDischarge, MCharge, MStorage)
-        solution.MDeficit, solution.MSpillage = (MDeficit, MSpillage)
-
-    return np.stack((FQ, NQ, NS, NV, AS, SW, TV), axis=1)
+    return np.stack((solution.FQ, solution.NQ, solution.NS, solution.NV, solution.AS, solution.SW, solution.TV), axis=1)
--- Simulation.py
+++ Simulation.py
@@ -12,25 +12,21 @@ def Reliability(solution, flexible):
 
     Netload = (solution.MLoad.sum(axis=1) - solution.GPV.sum(axis=1) - solution.GWind.sum(axis=1) -
                solution.GBaseload.sum(axis=1) - flexible)
-    intervals = solution.intervals
 
     Pcapacity = solution.CPHP.sum() * 1000 # S-CPHP(j), GW to MW
     Scapacity = solution.CPHS * 1000 # S-CPHS(j), GWh to MWh
-    efficiency, resolution = solution.efficiency, solution.resolution 
 
-    solution.Discharge = np.zeros(intervals)
-    solution.Charge = np.zeros(intervals)
-    solution.Storage = np.zeros(intervals)
-    
-    solution.Storage[-1] =  0.5*Scapacity
-    for t in range(intervals):
-        solution.Discharge[t] = np.minimum(np.minimum(np.maximum(0, Netload[t]), Pcapacity), solution.Storage[t-1] / resolution)
-        solution.Charge[t] = np.minimum(np.minimum(-1 * np.minimum(0, Netload[t]), Pcapacity), (Scapacity - solution.Storage[t-1]) / efficiency / resolution)
-        solution.Storage[t] = solution.Storage[t-1] - solution.Discharge[t] * resolution + solution.Charge[t] * resolution * efficiency
+    solution.Discharge = np.zeros(solution.intervals)
+    solution.Charge = np.zeros(solution.intervals)
+    solution.Storage = np.zeros(solution.intervals)
+    solution.Storage[-1] = 0.5*Scapacity
+    for t in range(solution.intervals):
+        solution.Discharge[t] = np.minimum(np.minimum(np.maximum(0, Netload[t]), Pcapacity), solution.Storage[t-1] / solution.resolution)
+        solution.Charge[t] = np.minimum(np.minimum(-1 * np.minimum(0, Netload[t]), Pcapacity), (Scapacity - solution.Storage[t-1]) / solution.efficiency / solution.resolution)
+        solution.Storage[t] = solution.Storage[t-1] - solution.Discharge[t] * solution.resolution + solution.Charge[t] * solution.resolution * solution.efficiency
 
     solution.Deficit = np.maximum(Netload - solution.Discharge, 0)
     solution.Spillage = -1 * np.minimum(Netload + solution.Charge, 0)
-
     solution.flexible = flexible
 
     return solution.Deficit
