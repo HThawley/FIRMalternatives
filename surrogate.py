0d12628b0cdadfed5a55eb8d6b6c1a8e40b1467d
Harry Thawley <Harry.Thawley@anu.edu.au>
2025-07-11 16:00:23 +1000

    -

--- surrogate.py
+++ surrogate.py
@@ -232,7 +232,7 @@ if __name__=="__main__":
     
     input_data = pd.read_csv(
         CSV_FILE_PATH, 
-        skiprows = 0,
+        skiprows = 4_000_000,
         nrows=20_000, 
         header=None
         ).to_numpy()
