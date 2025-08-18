5cdfffa07b941a07a294dec1831180ece66771d7
Harry Thawley <Harry.Thawley@anu.edu.au>
2025-08-18 12:26:15 +1000

    -

--- SurrogateValidation.py
+++ SurrogateValidation.py
@@ -7,7 +7,7 @@ Created on Mon Jul 28 14:24:00 2025
 import sys
 if "win" not in sys.platform:
     import matplotlib 
-    matplotlib.use("Agg")
+    matplotlib.use("TkAgg")
 
 import numpy as np
 import pandas as pd
